789fd1480ef18c706166f016f40195610bb3c017
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2022-10-22 08:15:28 -0700

    v0.6.1

---
+++ pcmon.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Pcmon v0.6.1\npcmon is not an actual command. Here is a list of commands that you can use:\n");
+    printf("dgraph:     monitor disk activity\n");
+    printf("mgraph:     Graph memory usage\n");
+    printf("pmgraph:    Graph process memory usage\n");
+    printf("cpugraph:   Graph CPU usage\n");
+    printf("pcpugraph:  Graph process CPU usage\n");
+}
